17fcb378fdd9ac2f3def08f0d266da4f0d75936e
rondeaumatias <mrondeau@ignetworks.com>
2018-08-18 12:36:20 -0300

    Progress

--- mysite/Profile/views.py
+++ mysite/Profile/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render,redirect
 
 
 def index(request):
-    return redirect('/profile')
+    return render(request, 'dashboard/profile_dashboard_section.html', {})
+    #return redirect('/profile')
 
 def profile(request):
     return render(request, 'dashboard/profile_dashboard_section.html', {})
