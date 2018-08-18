f5c7136f3bfcc6058ac69ee1ee64ad6d8daff651
rondeaumatias <mrondeau@ignetworks.com>
2018-08-18 20:09:05 -0300

    Progress

--- mysite/Profile/views.py
+++ mysite/Profile/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render
+from .models import Professional
 
 
 def index(request):
     return render(request, 'dashboard/profile_dashboard_section.html', {})
-    #return redirect('/profile')
+
+
+def get_proffesionals(request):
+    '''
+    Funcion que retorna todos los Profesionales de la base de datos.
+    :param request:
+    :return: Queryset Professionals
+    '''
+
+    try:
+        return render(
+            request,
+            'dashboard/profile/profile_professionals_section.html',
+            {'professionals': Professional.objects.all()}
+        )
+    except Professional.DoesNotExist:
+        return render(
+            request,
+            'dashboard/profile_dashboard_section.html'
+        )
